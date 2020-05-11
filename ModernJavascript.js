0fe77a34c7ba2a087620e8715601faa92b66994b
Hung Ly <hunghly@gmail.com>
2020-05-10 20:43:21 -0500

    added modules

--- ModernJavascript.js
+++ ModernJavascript.js
@@ -1,8 +1,20 @@
+import {steamKey} from './keys.js';
+let request = requires('request');
+
 // ES 6 updates
 // String Interpolation/String Template Literal
 let age = 26;
 let finalString = `Hung is ${age} years old`;
 let zenva = 'Zenva';
-zenva = zenva.padEnd(10, ' ');
-zenva = zenva.padStart(15, ' ');
+// Pad start and pad end
+zenva = zenva.padEnd(10, '.');
+zenva = zenva.padStart(15, '.');
 console.log(zenva);
+console.log(steamKey);
+
+let appId = '221380'; // AoE II game ID
+// let request = new Request('http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=440');
+
+// fetch(request).then((data) => {
+//   console.log(data);
+// })
