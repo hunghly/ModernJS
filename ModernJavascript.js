f03e84073e598ec455a97cac888722d067aa69e4
Hung Ly <hunghly@gmail.com>
2020-05-11 14:12:45 -0500

    finished classes

--- ModernJavascript.js
+++ ModernJavascript.js
@@ -37,25 +37,63 @@ promise.then((response) => {
 async function multiply(x,y) {
   return await x * y;
 }
-
 let appId = 221380; // AoE II game ID
-let url = `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${appId}`
-
+let url = `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${appId}`;
 async function fetchData(url) {
   console.log(url);
   let response = await fetch(url);
   let jsonResponse = await response.json();
+  printData(jsonResponse);
   console.log(JSON.stringify(jsonResponse));
+}
+// new loops
+let numbers = [1,2,3,4,5];
+for (let number of numbers) {
+  console.log('counting' + number);
+}
+
+let printData = (data) => {
+  let jsonObject = data['achievementpercentages'];
+  let allAchievements = jsonObject['achievements'];
+  let achievementsArray = [];
+  for (let achievement of allAchievements) {
+    // console.log('NAME: ' + achievement.name);
+    // console.log('PERCENT: ' + achievement.percent);
+    let newAchievement = new Achievement(achievement.name, achievement.percent);
+    newAchievement.printValues();
+    achievementsArray.push(newAchievement);
+  }
+  console.log(achievementsArray.length);
+  return achievementsArray;
 }
 
 fetchData(url).catch(()=>{
   console.log('error with fetching');
 });
 
-let inventory = {
-  'food': 2,
-  'clothing': 3
+// Classes
+class Achievement {
+
+    constructor(name, percent) {
+      this.name = name;
+      this.percent = percent;
+    }
+    printValues() {
+      console.log(`${this.name} achievement has been completed by ${this.percent} percentage of people.`);
+    }
 }
 
-console.log(inventory.food);
-console.log(inventory['food']);
+let myAchievement = new Achievement('hungWins', 50);
+console.log(myAchievement.name);
+console.log(myAchievement.percent);
+
+
+
+
+// let inventory = {
+//   'food': 2,
+//   'clothing': 3
+// }
+//
+// console.log(inventory.food);
+// console.log(inventory['food']);
