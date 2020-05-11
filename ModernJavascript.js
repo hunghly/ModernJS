b94aaf3215d95729d2b7993ef4a1825f5806dab4
Hung Ly <hunghly@gmail.com>
2020-05-10 20:09:57 -0500

    added git ignore

--- ModernJavascript.js
+++ ModernJavascript.js
@@ -1,2 +1,8 @@
-console.log('Hello World!');
-console.log('Hello Hung!');
+// ES 6 updates
+// String Interpolation/String Template Literal
+let age = 26;
+let finalString = `Hung is ${age} years old`;
+let zenva = 'Zenva';
+zenva = zenva.padEnd(10, ' ');
+zenva = zenva.padStart(15, ' ');
+console.log(zenva);
